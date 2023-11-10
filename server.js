11eeb076606ce8aa840738ad3ce876db90e34a51
Sarvajit Gupta <91364026+sarvajit2002@users.noreply.github.com>
2023-11-10 16:36:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ import authRoutes from './routes/authRoutes.js';
 import ProductRoutes from './routes/productRoutes.js'
 import cors from 'cors'
 import path from 'path';
+import {fileURLToPath} from 'url';
 dotenv.config();
 connectDB();
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename)
 const app = express()
 app.use(cors())
 app.use(morgan('dev'))

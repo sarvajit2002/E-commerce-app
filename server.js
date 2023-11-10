4452d44ca174e6f078106b2b3353e9202de01258
Sarva <sarvajitgupta@gmail.com>
2023-11-10 16:24:51 +0530

    Made changes

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ import connectDB from "./config/db.js";
 import authRoutes from './routes/authRoutes.js';
 import ProductRoutes from './routes/productRoutes.js'
 import cors from 'cors'
+import path from 'path';
 dotenv.config();
 connectDB();
 const app = express()
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(express.static(path.join(__dirname,'./client/build')))
 app.use("/api/v1/auth",authRoutes)
 app.use("/api/v1",ProductRoutes)
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 app.get('/',(req,res)=>{
     res.send("<h1>Welcome to ecommerce app</h1>")
 })
